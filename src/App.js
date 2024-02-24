686decb7149064c0372ac38f20cd3f4abc6cf112
siddharth <siddharth@scaler.com>
2024-02-24 14:10:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import Row from "./Row";
+import requests from './requests';
 // import {a,b} from "./Row";
 //  destructuring
 // {a:{}, b:{}}
+
 function App() {
-  // console.log(a,b);
   return (
     <div className="App">
-        <Row title="Netflix orignals" name="x"/>
-        <Row title="Movies" name="y"/>
-        <Row title="Tv Shows" name="z"/>
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title="Trending" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />    
     </div>
   );
 }
@@ -24,3 +30,11 @@ export default App;
 // reusable block of code 
 // html written in js is known as JSX
 // App(a)
+
+
+//  what is ajax 
+// api - is a way for your backend and frontend to talk to each other
+// ajax - asyncronus javascript and xml
+// fetch 
+// axios is a package on top of ajax which gives you a way to do ajax calls on a particular url
+
--- src/Row.js
+++ src/Row.js
@@ -1,26 +1,84 @@
-function Row({title,name}){
-    return (
-        <div>
-            <h2>
-                {title}
-            </h2>
-            <h3>
-                {name}
-            </h3>
+import axios from "./axios";
+import {useState, useEffect} from "react";
+import "./Row.css";
+const base_url = "https://image.tmdb.org/t/p/original"
+
+function Row({title,fetchUrl}){
+  // let response = [];
+  let [movies, setMovies] = useState([]);
+  // console.log(requests.fetchTrending);
+  useEffect(()=> {
+    axios.get(fetchUrl).then((response) => {
+      console.log(response.data.results);
+      // response = data;
+      setMovies(response.data.results);
+    })
+  }, [])
+  
+  
+  // api call using fetch or axios 
+  // ajax call 
+  // aysn
+  // promise 
+  // then 
+  
+  
+  return (
+    <div className="row">
+    <h2>
+      {title}
+    </h2>
+    
+    <div className="row_posters">
+    {          
+      movies.map((item) => {
+        return (
+          <img    
+          className= "row_poster"
+          src={`${base_url}${item.poster_path}`}
+          />)
+        })}
         </div>
-    )
-}
-
-// export let a = 10;
-// export let b = 20;
-
-//these exports which are named exports are exported as an object
-
-export default Row;
-// default export in a file can only be one 
-
-
-// two types of exports 
-// Default 
-// Named exports
-
+        
+        </div>
+        )
+      }
+      
+      // export let a = 10;
+      // export let b = 20;
+      
+      //these exports which are named exports are exported as an object
+      
+      export default Row;
+      // default export in a file can only be one 
+      
+      
+      // two types of exports 
+      // Default 
+      // Named exports
+      
+      // state variable
+      // that once set would cause a rerender of your react application
+      // how to create this state variable
+      
+      // useState hook
+      // useState(default value)
+      // returns [stateVar, setterMethod];
+      
+      // useEffect
+      // useEffect(function, [])
+      // useEffect(function)
+      // useEffect(function, [movies])
+      
+      // movies = 1
+      // movies = 2
+      // prev movies == cur movies false
+      // state variable to check the change of them
+      // lifecycle of a component 
+      // component when it is rendered for the first time mounting
+      // component is rendered again and again after that it is know rerendering
+      
+// assignment
+      // add youtube trailer on click 
+      // map on the app.js for rows
+      // poster size for the first row to be bigger and rest to be smaller
